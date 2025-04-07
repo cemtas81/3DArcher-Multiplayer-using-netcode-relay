b0e2602b90d38cbbac84bee27a154df3dfe0bc8f
cem <cemtas81@gmail.com>
2025-04-07 13:24:24 +0300

    lobby / dotween

---
+++ Assets/Scripts/Multiplayer/RelayManager.cs
@@ -0,0 +1,12 @@
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class RelayManager : MonoBehaviour
+{
+   
+}
